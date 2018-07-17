5430b28a8b452f97fcd84ebc07f8de48fea26bcc
skaz07 <skaz07@inbox.ru>
2018-07-17 21:21:59 +0300

    finished

--- calcutater-mvvm/ViewModel/CalcutaterModel.cs
+++ calcutater-mvvm/ViewModel/CalcutaterModel.cs
@@ -6,53 +6,145 @@ namespace calcutater_mvvm.ViewModel
 {
     public class CalcutaterModel : INotifyPropertyChanged
     {
-        private string _number;
-        public string Number
+
+
+
+        public bool Operation { get; set;}
+        bool _needClear;
+
+        // свойство обрабатывающее нажатие на кнопки с данными
+        private string _date;
+        public string Date
         {
-            get { return _number; }
+            get { return _date; }
             set
             {
-                _number = _number+ value;
+                _date = value;
                 OnPropertyChanged();
             }
         }
 
-        private int _operation;
-        public int Operation
+        private double _temp;
+        private string _rez;
+
+        //нажатие на цифры ←
+        private RelayCommand _back;
+        public RelayCommand Back
         {
-            get { return _operation; }
-            set
+            get
             {
-                _operation=Int32.Parse(_number);
+                return _back ??
+                  (_back = new RelayCommand(obj =>
+                  {
+                      Date = _date.Substring(0, _date.Length - 1);
+                  }));
             }
         }
-          
 
 
-    //нажатие на цифры
-    private RelayCommand numberButton;
+        //нажатие на цифры
+        private RelayCommand _numberButton;
         public RelayCommand NumberButton
         {
             get
             {
-                return numberButton ??
-                  (numberButton = new RelayCommand(obj =>
+                return _numberButton ??
+                  (_numberButton = new RelayCommand(obj =>
                   {
-                      Number = obj as string;
+                      if (_needClear == true) _date = "";
+                      Date = _date+ obj as string;
+                      _needClear = false;
                   }));
             }
         }
 
         // нажатие на С
-        private RelayCommand press_с;
+        private RelayCommand _press_с;
         public RelayCommand Press_с
         {
             get
             {
-                return numberButton ??
-                  (numberButton = new RelayCommand(obj =>
+                return _press_с ??
+                  (_press_с = new RelayCommand(obj =>
                   {
-                      Number = "";
+                      Date = "";
+                  }));
+            }
+        }
+
+
+        // нажатие на +
+        private RelayCommand _plus;
+        public RelayCommand Press_plus
+        {
+            get
+            {
+                return _plus ??
+                  (_plus = new RelayCommand(obj =>
+                  {
+                      if (_rez == null)
+                      {
+                          _temp =  double.Parse(_date);
+                          Date = "";
+                          _rez = _temp.ToString();
+                          Operation = true;
+                      }
+                      else
+                      {
+                          Date= (_temp+ double.Parse(_date)).ToString();
+                          _needClear = true;
+                      }
+                  }));
+            }
+        }
+
+
+
+        private RelayCommand _minus;
+        public RelayCommand Press_minus
+        {
+            get
+            {
+                return _minus ??
+                  (_minus = new RelayCommand(obj =>
+                  {
+                      if (_rez == null)
+                      {
+                          _temp = double.Parse(_date);
+                          Date = "";
+                          _rez = _temp.ToString();
+                          Operation = false;
+                      }
+                      else
+                      {
+                          Date = (_temp - double.Parse(_date)).ToString();
+                          _needClear = true;
+                      }
+                  }));
+            }
+        }
+
+        private RelayCommand _equally;
+        public RelayCommand Press_equally
+        {
+            get
+            {
+                return _equally ??
+                  (_equally = new RelayCommand(obj =>
+                  {
+                      if (_rez != null)
+                          if (Operation == true)
+                          {
+                              Date = (_temp + double.Parse(_date)).ToString();
+                              _needClear = true;
+                              _rez = null;
+                          }
+                          else
+                          {
+                              Date = (_temp - double.Parse(_date)).ToString();
+                              _needClear = true;
+                              _rez = null;
+                          }
                   }));
             }
         }
@@ -63,7 +155,7 @@ namespace calcutater_mvvm.ViewModel
 
 
 
-
+        // Вспомогательные функции 
         public event PropertyChangedEventHandler PropertyChanged;
 
         public void OnPropertyChanged([CallerMemberName]string prop = "")
